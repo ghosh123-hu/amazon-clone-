2de303aeaac3b4f11f1fe81176139fcb1a3736dd
Atharv Ghosh <programmmariojs8@gmail.com>
2026-09-12 09:28:56 +0530

    Initial Repo

---
+++ script.js
@@ -0,0 +1,20 @@
+
+let siteName = "Amazon Clone";
+
+const maxItems = 5;
+
+
+alert("Welcome to " + siteName);
+
+
+let cartCount = 3;
+
+if (cartCount === 0) {
+    alert("Your cart is empty");
+} else if (cartCount < maxItems) {
+    alert("You have " + cartCount + " items in your cart");
+} else {
+    alert("Your cart is full");
+}
+
+console.log
